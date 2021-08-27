984f942269dd46290da543347b1d8f343f8f8c75
Ivan Carvajal Antiñapa <ivanmcaa@gmail.com>
2021-08-27 00:20:21 -0400

    Manifest for module

--- l10n_bo_country_state/__manifest__.py
+++ l10n_bo_country_state/__manifest__.py
@@ -1,35 +1,31 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "l10n_bo_country_state",
+    'name': "Bolivian Country State",
 
     'summary': """
-        Short (1 phrase/line) summary of the module's purpose, used as
-        subtitle on modules listing or apps.openerp.com""",
-
-    'description': """
-        Long description of module's purpose
+        Departments, Provinces, Municipalities for Bolivian Country State
     """,
 
-    'author': "My Company",
-    'website': "http://www.yourcompany.com",
+    'description': """
+        Departments, Provinces, Municipalities for Bolivian Country State.
+    """,
+
+    'author': "Labcit Inc.",
+    'website': "http://www.labcit.com",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
-    'category': 'Uncategorized',
-    'version': '0.1',
+    'category': 'Localization',
+    'version': '14.0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base'],
+    'depends': ['contacts'],
 
     # always loaded
     'data': [
         # 'security/ir.model.access.csv',
         'views/views.xml',
         'views/templates.xml',
-    ],
-    # only loaded in demonstration mode
-    'demo': [
-        'demo/demo.xml',
     ],
 }
